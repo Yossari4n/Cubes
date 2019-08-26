8555762c7f93102d73305981568d7cda6258bb95
Nerovski <j.stokowski97@gmail.com>
2019-08-27 01:30:35 +0200

    Moved vertices data out as global constexpr

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #pragma warning(push, 0)
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#pragma warning(pop)
+
 
 #include <iostream>
 #include <array>
+#pragma warning(pop)
 
 constexpr float PI = 3.1415926535897931f;
-
 
 /*************
  * Callbacks *
@@ -63,7 +63,7 @@ vec3 Normalize(const vec3& vec) {
 
     if (mag != 1.0f) { // TODO fix naive test
         return { vec[0] / mag, vec[1] / mag, vec[2] / mag };
-    }  else {
+    } else {
         return vec;
     }
 }
@@ -82,7 +82,7 @@ constexpr mat4 Mul(const mat4& first, const mat4& second) {
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
             for (int k = 0; k < 4; k++) {
-                result[i][j] += first[i][k] * second[k][j];
+                result[i][j] += static_cast<float>(first[i][k] * second[k][j]);
             }
         }
     }
@@ -107,14 +107,14 @@ mat4 LookAt(const vec3& pos, const vec3& target, const vec3& up) {
     const vec3 x_axis = Normalize(Cross(Normalize(up), z_axis));
     const vec3 y_axis = Cross(z_axis, x_axis);
 
-    mat4 translation {
+    mat4 translation{
         1.0f,    0.0f,    0.0f,    0.0f,
         0.0f,    1.0f,    0.0f,    0.0f,
         0.0f,    0.0f,    1.0f,    0.0f,
         -pos[0], -pos[1], -pos[2], 1.0f
     };
 
-    mat4 rotation {
+    mat4 rotation{
         x_axis[0], y_axis[0], z_axis[0], 0.0f,
         x_axis[1], y_axis[1], z_axis[1], 0.0f,
         x_axis[2], y_axis[2], z_axis[2], 0.0f,
@@ -167,10 +167,64 @@ const char* FragmentShaderSource =
 "}\n\0";
 
 
+/*************************
+ * Constants and globals *
+ *************************/
+constexpr GLfloat CubeVertices[] = {
+    // back
+    -0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f, -0.5f,
+     0.5f,  0.5f, -0.5f,
+     0.5f,  0.5f, -0.5f,
+    -0.5f,  0.5f, -0.5f,
+    -0.5f, -0.5f, -0.5f,
+
+    // front
+    -0.5f, -0.5f,  0.5f,
+     0.5f, -0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f,  0.5f,
+    -0.5f, -0.5f,  0.5f,
+
+    // left
+    -0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f, -0.5f,
+    -0.5f, -0.5f, -0.5f,
+    -0.5f, -0.5f, -0.5f,
+    -0.5f, -0.5f,  0.5f,
+    -0.5f,  0.5f,  0.5f,
+
+    // right
+     0.5f,  0.5f,  0.5f,
+     0.5f,  0.5f, -0.5f,
+     0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+
+     // down
+     -0.5f, -0.5f, -0.5f,
+      0.5f, -0.5f, -0.5f,
+      0.5f, -0.5f,  0.5f,
+      0.5f, -0.5f,  0.5f,
+     -0.5f, -0.5f,  0.5f,
+     -0.5f, -0.5f, -0.5f,
+
+     // top
+     -0.5f,  0.5f, -0.5f,
+      0.5f,  0.5f, -0.5f,
+      0.5f,  0.5f,  0.5f,
+      0.5f,  0.5f,  0.5f,
+     -0.5f,  0.5f,  0.5f,
+     -0.5f,  0.5f, -0.5f
+};
+
+
 /***************************************
  * Visualizations forward declarations *
  ***************************************/
-void CubeWave(Window* window, GLuint shader_program);
+void CubeWave(const Window* window, GLuint shader_program);
 
 
 int main() {
@@ -185,7 +239,7 @@ int main() {
 #endif
 
     // Create window
-    Window window = { glfwCreateWindow(800, 600, "Cubes!", nullptr, nullptr), 800, 600 };
+    const Window window = { glfwCreateWindow(800, 600, "Cubes!", nullptr, nullptr), 800, 600 };
     if (!window.handler) {
         std::cout << "Failed to initialize window\n";
         glfwTerminate();
@@ -204,9 +258,9 @@ int main() {
     }
 
     // Shader program
-    GLuint vertex_shader = CreateShader(VertexShaderSource, GL_VERTEX_SHADER);
-    GLuint fragment_shader = CreateShader(FragmentShaderSource, GL_FRAGMENT_SHADER);
-    GLuint shader_program = CreateProgram(vertex_shader, fragment_shader);    
+    const GLuint vertex_shader = CreateShader(VertexShaderSource, GL_VERTEX_SHADER);
+    const GLuint fragment_shader = CreateShader(FragmentShaderSource, GL_FRAGMENT_SHADER);
+    const GLuint shader_program = CreateProgram(vertex_shader, fragment_shader);
     glDeleteShader(vertex_shader);
     glDeleteShader(fragment_shader);
 
@@ -219,7 +273,7 @@ int main() {
 }
 
 
-void CubeWave(Window* window, GLuint shader_program) {
+void CubeWave(const Window* window, GLuint shader_program) {
     constexpr int ROWS = 15;
     constexpr int COLUMNS = 15;
     constexpr float MIN_CUBE_HEIGHT = 5.0f;
@@ -227,82 +281,85 @@ void CubeWave(Window* window, GLuint shader_program) {
     constexpr float SIN_MULTIPLIER = 2.0f;
 
     // Verticies
-    constexpr float cube[] = {
-        // coords             color
+    constexpr GLfloat colors[] = {
         // back
-        -0.5f, -0.5f, -0.5f,  1.0f,  1.0f,  1.0f,
-         0.5f, -0.5f, -0.5f,  1.0f,  1.0f,  1.0f,
-         0.5f,  0.5f, -0.5f,  1.0f,  1.0f,  1.0f,
-         0.5f,  0.5f, -0.5f,  1.0f,  1.0f,  1.0f,
-        -0.5f,  0.5f, -0.5f,  1.0f,  1.0f,  1.0f,
-        -0.5f, -0.5f, -0.5f,  1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
 
         // front
-        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  0.18f,
-         0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  0.18f,
-         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  0.18f,
-         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  0.18f,
-        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  0.18f,
-        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  0.18f,
+        0.0f,  0.0f,  0.18f,
+        0.0f,  0.0f,  0.18f,
+        0.0f,  0.0f,  0.18f,
+        0.0f,  0.0f,  0.18f,
+        0.0f,  0.0f,  0.18f,
+        0.0f,  0.0f,  0.18f,
 
         // left
-        -0.5f,  0.5f,  0.5f,  1.0f,  1.0f,  1.0f,
-        -0.5f,  0.5f, -0.5f,  1.0f,  1.0f,  1.0f,
-        -0.5f, -0.5f, -0.5f,  1.0f,  1.0f,  1.0f,
-        -0.5f, -0.5f, -0.5f,  1.0f,  1.0f,  1.0f,
-        -0.5f, -0.5f,  0.5f,  1.0f,  1.0f,  1.0f,
-        -0.5f,  0.5f,  0.5f,  1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
 
         // right
-         0.5f,  0.5f,  0.5f,  0.65f,  0.8f,  0.6f,
-         0.5f,  0.5f, -0.5f,  0.65f,  0.8f,  0.6f,
-         0.5f, -0.5f, -0.5f,  0.65f,  0.8f,  0.6f,
-         0.5f, -0.5f, -0.5f,  0.65f,  0.8f,  0.6f,
-         0.5f, -0.5f,  0.5f,  0.65f,  0.8f,  0.6f,
-         0.5f,  0.5f,  0.5f,  0.65f,  0.8f,  0.6f,
+        0.65f,  0.8f,  0.6f,
+        0.65f,  0.8f,  0.6f,
+        0.65f,  0.8f,  0.6f,
+        0.65f,  0.8f,  0.6f,
+        0.65f,  0.8f,  0.6f,
+        0.65f,  0.8f,  0.6f,
 
-         // down
-         -0.5f, -0.5f, -0.5f,  1.0f,  1.0f,  1.0f,
-          0.5f, -0.5f, -0.5f,  1.0f,  1.0f,  1.0f,
-          0.5f, -0.5f,  0.5f,  1.0f,  1.0f,  1.0f,
-          0.5f, -0.5f,  0.5f,  1.0f,  1.0f,  1.0f,
-         -0.5f, -0.5f,  0.5f,  1.0f,  1.0f,  1.0f,
-         -0.5f, -0.5f, -0.5f,  1.0f,  1.0f,  1.0f,
+        // down
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
 
-         // top
-         -0.5f,  0.5f, -0.5f,  0.4f,  0.6f,  0.65f,
-          0.5f,  0.5f, -0.5f,  0.4f,  0.6f,  0.65f,
-          0.5f,  0.5f,  0.5f,  0.4f,  0.6f,  0.65f,
-          0.5f,  0.5f,  0.5f,  0.4f,  0.6f,  0.65f,
-         -0.5f,  0.5f,  0.5f,  0.4f,  0.6f,  0.65f,
-         -0.5f,  0.5f, -0.5f,  0.4f,  0.6f,  0.65f
+        // top
+        0.4f,  0.6f,  0.65f,
+        0.4f,  0.6f,  0.65f,
+        0.4f,  0.6f,  0.65f,
+        0.4f,  0.6f,  0.65f,
+        0.4f,  0.6f,  0.65f,
+        0.4f,  0.6f,  0.65f
     };
 
     // Buffer objects
-    GLuint VBO, VAO;
-    glGenBuffers(1, &VBO);
-    glGenVertexArrays(1, &VAO);
+    GLuint vertex_buffer, color_buffer, vao;
+    glGenBuffers(1, &vertex_buffer);
+    glGenBuffers(1, &color_buffer);
+    glGenVertexArrays(1, &vao);
 
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-    glBufferData(GL_ARRAY_BUFFER, sizeof(cube), cube, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_TRUE, 6 * sizeof(float), (void*)0);
+    glBindVertexArray(vao);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_TRUE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(CubeVertices), CubeVertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_TRUE, 0, (void*)0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, color_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_TRUE, 0, (void*)0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
     // Camera
     const mat4 projection = Perspective(45.0f, static_cast<float>(window->width / window->height), 0.1f, 100.0f);
-    const mat4 view = LookAt({ 20.0f, 22.5f, 20.0f }, 
+    const mat4 view = LookAt({ 20.0f, 22.5f, 20.0f },
                              { 0.0f, 0.0f, 0.0f },
                              { 0.0f, 1.0f, 0.0f });
 
-    mat4 pv = Mul(view, projection);
-    GLint pv_loc = glGetUniformLocation(shader_program, "pv");
+    const mat4 pv = Mul(view, projection);
+    const GLint pv_loc = glGetUniformLocation(shader_program, "pv");
 
     // Load uniforms
     glUseProgram(shader_program);
@@ -317,15 +374,15 @@ void CubeWave(Window* window, GLuint shader_program) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glUseProgram(shader_program);
-        glBindVertexArray(VAO);
+        glBindVertexArray(vao);
 
-        float time = static_cast<float>(glfwGetTime());
+        const float time = static_cast<float>(glfwGetTime());
         for (int i = -ROWS / 2; i < ROWS / 2; ++i) {
             for (int j = -COLUMNS / 2; j < COLUMNS / 2; ++j) {
-                float distance_factor = static_cast<float>(sqrt(pow(i, 2) + pow(j, 2))) * 0.9f;
-                float height = CUBE_HEIGHT_MULTIPLIER * sin(SIN_MULTIPLIER * time + distance_factor) + MIN_CUBE_HEIGHT;
+                const float distance_factor = static_cast<float>(sqrt(pow(i, 2) + pow(j, 2))) * 0.9f;
+                const float height = CUBE_HEIGHT_MULTIPLIER * sin(SIN_MULTIPLIER * time + distance_factor) + MIN_CUBE_HEIGHT;
 
-                mat4 model {
+                mat4 model{
                     1.0f, 0.0f, 0.0f, 0.0f,
                     0.0f, 1.0f, 0.0f, 0.0f,
                     0.0f, 0.0f, 1.0f, 0.0f,
@@ -333,10 +390,10 @@ void CubeWave(Window* window, GLuint shader_program) {
                 };
                 model = Translate(model, { static_cast<float>(i), 0.0f, static_cast<float>(j) });
                 model = Scale(model, { 1.0f, height, 1.0f });
-                GLint model_loc = glGetUniformLocation(shader_program, "model");
+                const GLint model_loc = glGetUniformLocation(shader_program, "model");
                 glUniformMatrix4fv(model_loc, 1, GL_FALSE, &model[0][0]);
 
-                glDrawArrays(GL_TRIANGLES, 0, sizeof(cube) / sizeof(cube[0]));
+                glDrawArrays(GL_TRIANGLES, 0, 36);
             }
         }
 
@@ -345,6 +402,7 @@ void CubeWave(Window* window, GLuint shader_program) {
     }
 
     // Free memory
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vertex_buffer);
+    glDeleteBuffers(1, &color_buffer);
 }
