bc459586f78b2395eb484cf2ff84e87312309bc0
Yossari4n <jakubstokowski@tutanota.com>
2022-01-16 16:26:30 +0100

    Remove all dependencies

--- Cubes/main.cpp
+++ Cubes/main.cpp
@@ -1,40 +1,84 @@
-#pragma warning(push, 0)
-#include <glad/glad.h>
-#include <glfw/glfw3.h>
-#pragma warning(pop)
+#include <Windows.h>
+#include <gl/GL.h>
+#include "glext.h"
+#include "wglext.h"
 
+#include <chrono>
 #include <iostream>
 #include <array>
+
+#undef near
+#undef far
 
 /********************************
  * OpenGL utilities and helpers *
  ********************************/
-void FramebufferSizeCallback(GLFWwindow* window, int width, int height) {
-    (void*)window;
-    glViewport(0, 0, width, height);
+ template <class T>
+ void LoadOpenGLProc(T& procPointer, const char* name) {
+    procPointer = reinterpret_cast<T>(wglGetProcAddress(name));
+    if(!procPointer) {
+        OutputDebugString("Failed to load function:\n\t");
+        OutputDebugString(name);
+        OutputDebugString("\n");
+        PostQuitMessage(0);
+    }
+ }
+
+PFNWGLCHOOSEPIXELFORMATARBPROC wglChoosePixelFormatARB = nullptr;
+PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribsARB = nullptr;
+PFNGLCREATEPROGRAMPROC wglCreateProgram = nullptr;
+PFNGLATTACHSHADERPROC wglAttachShader = nullptr;
+PFNGLLINKPROGRAMPROC wglLinkProgram = nullptr;
+PFNGLCREATESHADERPROC wglCreateShader = nullptr;
+PFNGLSHADERSOURCEPROC wglShaderSource = nullptr;
+PFNGLCOMPILESHADERPROC wglCompileShader = nullptr;
+PFNGLDELETESHADERPROC wglDeleteShader = nullptr;
+PFNGLGENBUFFERSARBPROC wglGenBuffers = nullptr;
+PFNGLGENVERTEXARRAYSPROC wglGenVertexArrays = nullptr;
+PFNGLBINDVERTEXARRAYPROC wglBindVertexArray = nullptr;
+PFNGLBUFFERDATAPROC wglBufferData = nullptr;
+PFNGLENABLEVERTEXATTRIBARRAYPROC wglEnableVertexAttribArray = nullptr;
+PFNGLBINDBUFFERPROC wglBindBuffer = nullptr;
+PFNGLVERTEXATTRIBPOINTERPROC wglVertexAttribPointer = nullptr;
+PFNGLGETUNIFORMLOCATIONPROC wglGetUniformLocation = nullptr;
+PFNGLUSEPROGRAMPROC wglUseProgram = nullptr;
+PFNGLUNIFORMMATRIX4FVPROC wglUniformMatrix4fv = nullptr;
+PFNGLDELETEVERTEXARRAYSPROC wglDeleteVertexArrays = nullptr;
+PFNGLDELETEBUFFERSPROC wglDeleteBuffers = nullptr;
+
+LRESULT CALLBACK WindowCallback(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) {
+    switch(Msg) {
+        case WM_CLOSE:
+            PostQuitMessage(0);
+            break;
+
+        default:
+            return DefWindowProc(hWnd, Msg, wParam, lParam);
+    }
+
+    return 0;
 }
 
-struct Window {
-    GLFWwindow* const handler;
-    const int width;
-    const int height;
-};
-
 GLuint CreateShader(const char* source, GLenum shader_type) {
-    const GLuint shader = glCreateShader(shader_type);
-    glShaderSource(shader, 1, &source, nullptr);
-    glCompileShader(shader);
+    const GLuint shader = wglCreateShader(shader_type);
+    wglShaderSource(shader, 1, &source, nullptr);
+    wglCompileShader(shader);
     return shader;
 }
 
 GLuint CreateProgram(GLuint vertex_shader, GLuint fragment_shader) {
-    const GLuint program = glCreateProgram();
-    glAttachShader(program, vertex_shader);
-    glAttachShader(program, fragment_shader);
-    glLinkProgram(program);
+    const GLuint program = wglCreateProgram();
+    wglAttachShader(program, vertex_shader);
+    wglAttachShader(program, fragment_shader);
+    wglLinkProgram(program);
     return program;
 }
 
+static const auto startTime = std::chrono::high_resolution_clock::now();
+float GetTime() {
+    const auto currentTime = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - startTime).count() / 1000.0f;
+}
 
 /******************************
  * Math utilities and helpers *
@@ -225,55 +269,184 @@ constexpr GLfloat CubeVertices[] = {
 /***************************************
  * Visualizations forward declarations *
  ***************************************/
-void CubeWave(const Window* window, GLuint shader_program);
-void PenroseStairs(const Window* window, GLuint shader_program);
+void CubeWave(HDC deviceContext, GLuint shader_program);
+// void PenroseStairs(const Window* window, GLuint shader_program);
 
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow) {
+    WNDCLASSEX wcex;
+    ZeroMemory(&wcex, sizeof(wcex));
+    wcex.cbSize = sizeof(wcex);
+    wcex.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+    wcex.lpfnWndProc = WindowCallback;
+    wcex.hInstance = hInstance;
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.lpszClassName = "CubesWindowClass";
 
-int main() {
-    // Initialize glfw
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
-    // Create window
-    const Window window = { glfwCreateWindow(WindowWidth, WindowHeight, "Cubes!", nullptr, nullptr), WindowWidth, WindowHeight };
-    if (!window.handler) {
-        std::cout << "Failed to initialize window\n";
-        glfwTerminate();
-        return EXIT_FAILURE;
+    LPTSTR windowClass = MAKEINTATOM(RegisterClassEx(&wcex));
+    if(!windowClass) {
+        OutputDebugString("Failed to register window class");
+        PostQuitMessage(0);
     }
-    glfwMakeContextCurrent(window.handler);
 
-    // Set callbacks
-    glfwSetFramebufferSizeCallback(window.handler, FramebufferSizeCallback);
+    // Fake ViewPort
+    HWND fakeWindow = CreateWindowEx(
+        0,                              // Optional window styles.
+        windowClass,                    // Window class
+        "Fake Viewport",                // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
 
-    // Load glad
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cout << "Failed to initialize GLAD\n";
-        glfwTerminate();
-        return EXIT_FAILURE;
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,       // Parent window
+        NULL,       // Menu
+        hInstance,  // Instance handle
+        NULL        // Additional application data
+    );
+
+    HDC fakeDeviceContext = GetDC(fakeWindow);
+
+    PIXELFORMATDESCRIPTOR fakePixelFormatDescriptor{};
+    ZeroMemory(&fakePixelFormatDescriptor, sizeof(fakePixelFormatDescriptor));
+    fakePixelFormatDescriptor.nSize = sizeof(fakePixelFormatDescriptor);
+    fakePixelFormatDescriptor.nVersion = 1;
+    fakePixelFormatDescriptor.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    fakePixelFormatDescriptor.iPixelType = PFD_TYPE_RGBA;
+    fakePixelFormatDescriptor.cColorBits = 32;
+    fakePixelFormatDescriptor.cAlphaBits = 8;
+    fakePixelFormatDescriptor.cDepthBits = 24;
+
+    const int fakePixelFormatDescriptorID = ChoosePixelFormat(fakeDeviceContext, &fakePixelFormatDescriptor);
+    if(fakePixelFormatDescriptorID == 0) {
+        OutputDebugString("Failed to choose fake pixel format");
+        PostQuitMessage(0);
+    }
+
+    if(!SetPixelFormat(fakeDeviceContext, fakePixelFormatDescriptorID, &fakePixelFormatDescriptor)) {
+        OutputDebugString("Failed to set fake pixel format");
+        PostQuitMessage(0);
+    }
+
+    HGLRC fakeRenderContext = wglCreateContext(fakeDeviceContext);
+    if(fakeRenderContext == 0) {
+        OutputDebugString("Failed to set fake pixel format");
+        PostQuitMessage(0);
+    }
+
+    if(!wglMakeCurrent(fakeDeviceContext, fakeRenderContext)) {
+        OutputDebugString("Failed to set fake pixel format");
+        PostQuitMessage(0);
+    }
+
+    LoadOpenGLProc<PFNWGLCHOOSEPIXELFORMATARBPROC>(wglChoosePixelFormatARB, "wglChoosePixelFormatARB");
+    LoadOpenGLProc<PFNWGLCREATECONTEXTATTRIBSARBPROC>(wglCreateContextAttribsARB, "wglCreateContextAttribsARB");
+    LoadOpenGLProc<PFNGLCREATEPROGRAMPROC>(wglCreateProgram, "glCreateProgram");
+    LoadOpenGLProc<PFNGLATTACHSHADERPROC>(wglAttachShader, "glAttachShader");
+    LoadOpenGLProc<PFNGLLINKPROGRAMPROC>(wglLinkProgram, "glLinkProgram");
+    LoadOpenGLProc<PFNGLCREATESHADERPROC>(wglCreateShader, "glCreateShader");
+    LoadOpenGLProc<PFNGLSHADERSOURCEPROC>(wglShaderSource, "glShaderSource");
+    LoadOpenGLProc<PFNGLCOMPILESHADERPROC>(wglCompileShader, "glCompileShader");
+    LoadOpenGLProc<PFNGLDELETESHADERPROC>(wglDeleteShader, "glDeleteShader");
+    LoadOpenGLProc<PFNGLGENBUFFERSARBPROC>(wglGenBuffers, "glGenBuffers");
+    LoadOpenGLProc<PFNGLGENVERTEXARRAYSPROC>(wglGenVertexArrays, "glGenVertexArrays");
+    LoadOpenGLProc<PFNGLBINDVERTEXARRAYPROC>(wglBindVertexArray, "glBindVertexArray");
+    LoadOpenGLProc<PFNGLBUFFERDATAPROC>(wglBufferData, "glBufferData");
+    LoadOpenGLProc<PFNGLENABLEVERTEXATTRIBARRAYPROC>(wglEnableVertexAttribArray, "glEnableVertexAttribArray");
+    LoadOpenGLProc<PFNGLBINDBUFFERPROC>(wglBindBuffer, "glBindBuffer");
+    LoadOpenGLProc<PFNGLVERTEXATTRIBPOINTERPROC>(wglVertexAttribPointer, "glVertexAttribPointer");
+    LoadOpenGLProc<PFNGLGETUNIFORMLOCATIONPROC>(wglGetUniformLocation, "glGetUniformLocation");
+    LoadOpenGLProc<PFNGLUSEPROGRAMPROC>(wglUseProgram, "glUseProgram");
+    LoadOpenGLProc<PFNGLUNIFORMMATRIX4FVPROC>(wglUniformMatrix4fv, "glUniformMatrix4fv");
+    LoadOpenGLProc<PFNGLDELETEVERTEXARRAYSPROC>(wglDeleteVertexArrays, "glDeleteVertexArrays");
+    LoadOpenGLProc<PFNGLDELETEBUFFERSPROC>(wglDeleteBuffers, "glDeleteBuffers");
+
+    // Real viewport
+    HWND window = CreateWindowEx(
+        0,                              // Optional window styles.
+        windowClass,                    // Window class
+        "Cubes!",                       // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
+
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,       // Parent window
+        NULL,       // Menu
+        hInstance,      // Instance handle
+        NULL        // Additional application data
+    );
+
+    HDC deviceContext = GetDC(window);
+    constexpr int pixelAttribs[] = {
+        WGL_DRAW_TO_WINDOW_ARB, GL_TRUE,
+        WGL_SUPPORT_OPENGL_ARB, GL_TRUE,
+        WGL_DOUBLE_BUFFER_ARB, GL_TRUE,
+        WGL_PIXEL_TYPE_ARB, WGL_TYPE_RGBA_ARB,
+        WGL_ACCELERATION_ARB, WGL_FULL_ACCELERATION_ARB,
+        WGL_COLOR_BITS_ARB, 32,
+        WGL_ALPHA_BITS_ARB, 8,
+        WGL_DEPTH_BITS_ARB, 24,
+        WGL_STENCIL_BITS_ARB, 8,
+        WGL_SAMPLE_BUFFERS_ARB, GL_TRUE,
+        WGL_SAMPLES_ARB, 4,
+        0
+    };
+
+    int pixelFormatID;
+    UINT numFormats;
+    const bool status = wglChoosePixelFormatARB(deviceContext, pixelAttribs, NULL, 1, &pixelFormatID, &numFormats);
+    if(status == false || numFormats == 0) {
+        OutputDebugString("Failed to choose pixel format ARB");
+        PostQuitMessage(0);
+    }
+
+    PIXELFORMATDESCRIPTOR pixelFormatDescriptor{};
+    DescribePixelFormat(deviceContext, pixelFormatID, sizeof(pixelFormatDescriptor), &pixelFormatDescriptor);
+    SetPixelFormat(deviceContext, pixelFormatID, &pixelFormatDescriptor);
+
+    const int majorMin = 4;
+    const int minorMin = 0;
+    const int contextAttribs[] = {
+            WGL_CONTEXT_MAJOR_VERSION_ARB, majorMin,
+            WGL_CONTEXT_MINOR_VERSION_ARB, minorMin,
+            WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
+            0
+    };
+
+    HGLRC renderContext = wglCreateContextAttribsARB(deviceContext, 0, contextAttribs);
+    if(renderContext == NULL) {
+        OutputDebugString("Failed to create context");
+        PostQuitMessage(0);
+    }
+
+    SetWindowText(window, "Cubes!");
+    ShowWindow(window, nCmdShow);
+
+    // Delete fake view
+    wglMakeCurrent(NULL, NULL);
+    wglDeleteContext(fakeRenderContext);
+    ReleaseDC(fakeWindow, fakeDeviceContext);
+    DestroyWindow(fakeWindow);
+    if(wglMakeCurrent(deviceContext, renderContext) == false) {
+        OutputDebugString("Failed to context current");
+        PostQuitMessage(0);
     }
 
     // Shader program
     const GLuint vertex_shader = CreateShader(VertexShaderSource, GL_VERTEX_SHADER);
     const GLuint fragment_shader = CreateShader(FragmentShaderSource, GL_FRAGMENT_SHADER);
     const GLuint shader_program = CreateProgram(vertex_shader, fragment_shader);
-    glDeleteShader(vertex_shader);
-    glDeleteShader(fragment_shader);
+    wglDeleteShader(vertex_shader);
+    wglDeleteShader(fragment_shader);
 
     // Different scenes
-    switch(1) {
+    switch(0) {
         case 0:
-            CubeWave(&window, shader_program);
+            CubeWave(deviceContext, shader_program);
             break;
 
         case 1:
-            PenroseStairs(&window, shader_program);
+            // PenroseStairs(&window, shader_program);
             break;
 
         default:
@@ -281,12 +454,23 @@ int main() {
     }
 
     // End of application
-    glfwTerminate();
+    wglMakeCurrent(NULL, NULL);
+    if(renderContext) {
+        wglDeleteContext(renderContext);
+    }
+
+    if(deviceContext) {
+        ReleaseDC(window, deviceContext);
+    }
+
+    if(window) {
+        DestroyWindow(window);
+    }
+
     return EXIT_SUCCESS;
 }
 
-
-void CubeWave(const Window* window, GLuint shader_program) {
+void CubeWave(HDC deviceContext, GLuint shader_program) {
     constexpr int ROWS = 15;
     constexpr int COLUMNS = 15;
     constexpr float MIN_CUBE_HEIGHT = 5.0f;
@@ -346,27 +530,27 @@ void CubeWave(const Window* window, GLuint shader_program) {
 
     // Buffer objects
     GLuint vertex_buffer, color_buffer, vao;
-    glGenBuffers(1, &vertex_buffer);
-    glGenBuffers(1, &color_buffer);
-    glGenVertexArrays(1, &vao);
-
-    glBindVertexArray(vao);
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
-
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(CubeVertices), CubeVertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_TRUE, 0, (void*)0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, color_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_TRUE, 0, (void*)0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
+    wglGenBuffers(1, &vertex_buffer);
+    wglGenBuffers(1, &color_buffer);
+    wglGenVertexArrays(1, &vao);
+    
+    wglBindVertexArray(vao);
+    wglEnableVertexAttribArray(0);
+    wglEnableVertexAttribArray(1);
+    
+    wglBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    wglBufferData(GL_ARRAY_BUFFER, sizeof(CubeVertices), CubeVertices, GL_STATIC_DRAW);
+    wglVertexAttribPointer(0, 3, GL_FLOAT, GL_TRUE, 0, (void*)0);
+    
+    wglBindBuffer(GL_ARRAY_BUFFER, color_buffer);
+    wglBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+    wglVertexAttribPointer(1, 3, GL_FLOAT, GL_TRUE, 0, (void*)0);
+    
+    wglBindBuffer(GL_ARRAY_BUFFER, 0);
+    wglBindVertexArray(0);
 
     // Camera
-    const mat4 projection = Perspective(45.0f, static_cast<float>(window->width / window->height), 0.1f, 100.0f);
+    const mat4 projection = Perspective(45.0f, static_cast<float>(WindowWidth / WindowHeight), 0.1f, 100.0f);
     const mat4 view = LookAt(
         { 20.0f, 22.5f, 20.0f },
         { 0.0f, 0.0f, 0.0f },
@@ -374,26 +558,36 @@ void CubeWave(const Window* window, GLuint shader_program) {
     );
 
     const mat4 pv = Mul(view, projection);
-    const GLint pv_loc = glGetUniformLocation(shader_program, "pv");
+    const GLint pv_loc = wglGetUniformLocation(shader_program, "pv");
 
     // Load uniforms
-    glUseProgram(shader_program);
-    glUniformMatrix4fv(pv_loc, 1, GL_FALSE, &pv[0][0]);
+    wglUseProgram(shader_program);
+    wglUniformMatrix4fv(pv_loc, 1, GL_FALSE, &pv[0][0]);
 
     // OpenGL settings
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     glEnable(GL_DEPTH_TEST);
 
-    // Render loop
-    while (!glfwWindowShouldClose(window->handler)) {
+    MSG msg;
+    bool shouldCloseWindow = false;
+    while(shouldCloseWindow == false) {
+        while(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+            if(msg.message == WM_QUIT) {
+                shouldCloseWindow = true;
+            }
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        // Rendering
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        glUseProgram(shader_program);
-        glBindVertexArray(vao);
+        wglUseProgram(shader_program);
+        wglBindVertexArray(vao);
 
-        const float time = static_cast<float>(glfwGetTime());
-        for (int i = -ROWS / 2; i < ROWS / 2; ++i) {
-            for (int j = -COLUMNS / 2; j < COLUMNS / 2; ++j) {
+        const float time = GetTime(); // static_cast<float>(glfwGetTime());
+        for(int i = -ROWS / 2; i < ROWS / 2; ++i) {
+            for(int j = -COLUMNS / 2; j < COLUMNS / 2; ++j) {
                 const float distance_factor = static_cast<float>(sqrt(pow(i, 2) + pow(j, 2))) * 0.9f;
                 const float height = CUBE_HEIGHT_MULTIPLIER * sin(SIN_MULTIPLIER * time + distance_factor) + MIN_CUBE_HEIGHT;
 
@@ -405,25 +599,25 @@ void CubeWave(const Window* window, GLuint shader_program) {
                 };
                 model = Translate(model, { static_cast<float>(i), 0.0f, static_cast<float>(j) });
                 model = Scale(model, { 1.0f, height, 1.0f });
-                const GLint model_loc = glGetUniformLocation(shader_program, "model");
-                glUniformMatrix4fv(model_loc, 1, GL_FALSE, &model[0][0]);
+                const GLint model_loc = wglGetUniformLocation(shader_program, "model");
+                wglUniformMatrix4fv(model_loc, 1, GL_FALSE, &model[0][0]);
 
                 glDrawArrays(GL_TRIANGLES, 0, 36);
             }
         }
 
-        glfwSwapBuffers(window->handler);
-        glfwPollEvents();
+        // Swap buffers
+        SwapBuffers(deviceContext);
     }
 
     // Free memory
-    glDeleteVertexArrays(1, &vao);
-    glDeleteBuffers(1, &vertex_buffer);
-    glDeleteBuffers(1, &color_buffer);
+    wglDeleteVertexArrays(1, &vao);
+    wglDeleteBuffers(1, &vertex_buffer);
+    wglDeleteBuffers(1, &color_buffer);
 }
 
 
-void PenroseStairs(const Window* window, GLuint shader_program) {
+/*void PenroseStairs(const Window* window, GLuint shader_program) {
     constexpr GLfloat colors[] = {
         // back
         0.37f, 0.0f,  0.73f,
@@ -563,4 +757,4 @@ void PenroseStairs(const Window* window, GLuint shader_program) {
     glDeleteVertexArrays(1, &vao);
     glDeleteBuffers(1, &vertex_buffer);
     glDeleteBuffers(1, &color_buffer);
-}
+}*/
